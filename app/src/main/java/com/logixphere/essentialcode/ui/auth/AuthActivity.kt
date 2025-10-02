2b0be9cc7d08794cb7639253362256769b7bb69d
Dika Alfarell <al.ramdhan312@gmail.com>
2025-10-02 23:47:03 +0700

    update sign in view

--- app/src/main/java/com/logixphere/essentialcode/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/logixphere/essentialcode/ui/auth/AuthActivity.kt
@@ -13,8 +13,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.GMobiledata
@@ -25,6 +27,7 @@ import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -34,12 +37,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -49,6 +55,9 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.logixphere.essentialcode.R
+import com.logixphere.essentialcode.ui.theme.Purple40
+import com.logixphere.essentialcode.utils.LPOutlButton
+import com.logixphere.essentialcode.utils.LPOutlTextField
 import com.logixphere.essentialcode.utils.LPTextField
 import com.logixphere.essentialcode.utils.ScreenUtil
 import com.logixphere.essentialcode.utils.ShowLoading
@@ -68,126 +77,141 @@ private fun LoginScreen(viewModel: AuthViewModel) {
 
     if(viewModel.loading.value == true) ShowLoading {}
 
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Box {
-            Image(
-                modifier = Modifier.fillMaxSize(),
-                painter = painterResource(R.drawable.bg_login),
-                contentDescription = "background_login",
-                contentScale = ContentScale.FillBounds
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 50.dp)
-                        .padding(horizontal = 30.dp),
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically,
-                        content = {
-                            Column(modifier = Modifier.width(ScreenUtil.getScreenWidth(.5f))) {
-                                Text(
-                                    "Login 1",
-                                    fontSize = TextUnit(
-                                        value = 32f,
-                                        type = TextUnitType.Sp
-                                    ),
-                                    fontFamily = FontFamily(
-                                        Font(R.font.nicomoji_regular, FontWeight.Normal)
-                                    )
-                                )
-                                Spacer(modifier = Modifier.height(4.dp))
-                                Text("Signin by doing it self and lorem ipsum")
-                            }
-                            Spacer(modifier = Modifier.width(30.dp))
-                            Image(
-                                modifier = Modifier.width(80.dp), painter = painterResource(
-                                    R.mipmap.ic_launcher_foreground
-                                ), contentDescription = "Logo App"
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.bg_login),
+            contentDescription = "background_login",
+            contentScale = ContentScale.FillBounds
+        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 32.dp),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                content = {
+                    Column(modifier = Modifier.width(ScreenUtil.getScreenWidth(.5f))) {
+                        Text(
+                            "Login 1",
+                            fontSize = TextUnit(
+                                value = 32f,
+                                type = TextUnitType.Sp
+                            ),
+                            fontFamily = FontFamily(
+                                Font(R.font.nicomoji_regular, FontWeight.Normal)
                             )
-                        }
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    LPTextField(
-                        value = loginState.userLogin,
-                        onValueChange = { viewModel.onEvent(LoginEvent.UserLoginChanged(it)) },
-                        label = "Username",
-                        isError = loginState.userLoginError,
-                        keyboardOptions = KeyboardOptions.Default.copy(
-                            keyboardType = KeyboardType.Email
                         )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text("Signin by doing it self and lorem ipsum")
+                    }
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Image(
+                        modifier = Modifier.width(80.dp), painter = painterResource(
+                            R.mipmap.ic_launcher_foreground
+                        ), contentDescription = "Logo App"
                     )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    LPTextField(
-                        value = loginState.userPwd,
-                        onValueChange = { viewModel.onEvent(LoginEvent.UserPWDChanged(it)) },
-                        label = "Password",
-                        isError = loginState.userPwdError,
-                        visualTransformation = if (loginState.pwdFieldVisibility) VisualTransformation.None else PasswordVisualTransformation(),
-                        trailingIcon = {
-                            val image = if (loginState.pwdFieldVisibility)
-                                Icons.Rounded.Visibility
-                            else
-                                Icons.Filled.VisibilityOff
-
-                            IconButton({ viewModel.changeVisiblePassword(loginState.pwdFieldVisibility) }) {
-                                Icon(image, "Visibility")
-                            }
-                        },
-                        keyboardOptions = KeyboardOptions.Default.copy(
-                            keyboardType = KeyboardType.Password
-                        )
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-
-                            },
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Checkbox(
-                            checked = false,
-                            onCheckedChange = {
-
-                            }
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text("Remember me")
-                    }
-                    OutlinedButton(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        onClick = {
-                        viewModel.onEvent(LoginEvent.Loading(true))
-                    }) {
-                        Text("Login")
-                    }
-                    Spacer(modifier = Modifier.height(30.dp))
-                    Text("Or sign in with")
-                    Spacer(modifier = Modifier.height(30.dp))
-                    FilledIconButton(
-                        onClick = {
-
-                        },
-                        shape = RoundedCornerShape(12.dp)
-                    ) {
-                        Icon(Icons.Default.GMobiledata, "google")
-                    }
                 }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            LoginField(loginState, viewModel)
+            Spacer(modifier = Modifier.height(10.dp))
+            PasswordField(loginState, viewModel)
+            Spacer(modifier = Modifier.height(10.dp))
+            RememberMe()
+            LPOutlButton(
+                onClick = {
+                    viewModel.onEvent(LoginEvent.Loading(true))
+                }
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Text("Or sign in with")
+            Spacer(modifier = Modifier.height(30.dp))
+            IconButton(
+                onClick = {
+
+                },
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(Purple40, shape = RoundedCornerShape(8.dp))
+                    .padding(8.dp)
+                    .border(width = 1.dp, color = Color.Transparent, shape = RoundedCornerShape(8.dp))
+            ) {
+                Icon(
+                    Icons.Default.GMobiledata,
+                    tint = Color.White,
+                    modifier = Modifier.size(50.dp),
+                    contentDescription = "google"
+                )
             }
         }
+    }
+}
+
+@Composable
+private fun LoginField(loginState: LoginEvent.LoginState, viewModel: AuthViewModel) {
+    val focusManager = LocalFocusManager.current
+    LPOutlTextField(
+        value = loginState.userLogin,
+        onValueChange = { viewModel.onEvent(LoginEvent.UserLoginChanged(it)) },
+        placeholder = "Username",
+        isError = loginState.userLoginError,
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Next
+        ),
+        keyboardActions = KeyboardActions(
+            onNext = { focusManager.moveFocus(FocusDirection.Down) }
+        )
+    )
+}
+
+@Composable
+private fun PasswordField(loginState: LoginEvent.LoginState, viewModel: AuthViewModel) {
+    LPOutlTextField(
+        value = loginState.userPwd,
+        onValueChange = { viewModel.onEvent(LoginEvent.UserPWDChanged(it)) },
+        placeholder = "Password",
+        isError = loginState.userPwdError,
+        visualTransformation = if (loginState.pwdFieldVisibility) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            val image = if (loginState.pwdFieldVisibility)
+                Icons.Rounded.Visibility
+            else
+                Icons.Filled.VisibilityOff
+
+            IconButton({ viewModel.changeVisiblePassword(loginState.pwdFieldVisibility) }) {
+                Icon(image, "Visibility")
+            }
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Password
+        )
+    )
+}
+
+@Composable
+private fun RememberMe() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = false,
+            onCheckedChange = {
+
+            }
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text("Remember me")
     }
 }
--- app/src/main/java/com/logixphere/essentialcode/utils/LPWidgets.kt
+++ app/src/main/java/com/logixphere/essentialcode/utils/LPWidgets.kt
@@ -1,22 +1,25 @@
 package com.logixphere.essentialcode.utils
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import com.logixphere.essentialcode.ui.theme.Purple40
+import com.logixphere.essentialcode.viewmodels.AuthViewModel
+import com.logixphere.essentialcode.viewmodels.LoginEvent
 
 @Composable
 fun LPTextField(
@@ -27,7 +30,8 @@ fun LPTextField(
     supportingText: @Composable (() -> Unit)? = null,
     visualTransformation: VisualTransformation = VisualTransformation.None,
     trailingIcon: @Composable (() -> Unit)? = null,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default
 ) {
     TextField(
         value = value,
@@ -45,5 +49,66 @@ fun LPTextField(
         visualTransformation = visualTransformation,
         trailingIcon = trailingIcon,
         keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
     )
+}
+
+@Composable
+fun LPOutlTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String? = null,
+    isError: Boolean = false,
+    label: String? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = Color(0xFFDEDEDE),
+            unfocusedContainerColor = Color(0xFFDEDEDE),
+            focusedBorderColor = Purple40,
+            unfocusedBorderColor = Color.Gray,
+            unfocusedTextColor = Color.Gray,
+            focusedTextColor = Color.Black
+        ),
+        enabled = true,
+        isError = isError,
+        label = if(label != null) {
+            { Text(label, style = TextStyle.Default.copy(background = Color.Transparent)) }
+        } else null,
+        shape = CircleShape,
+        placeholder = if(placeholder != null) {
+            { Text(placeholder) }
+        } else null,
+        supportingText = supportingText,
+        singleLine = true,
+        visualTransformation = visualTransformation,
+        trailingIcon = trailingIcon,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions
+    )
+}
+
+@Composable
+fun LPOutlButton(onClick: () -> Unit) {
+    OutlinedButton(
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = ButtonDefaults.outlinedButtonColors(
+            containerColor = Purple40,
+            contentColor = Color.White
+        ),
+        onClick = onClick
+    ) {
+        Text("Login")
+    }
 }
