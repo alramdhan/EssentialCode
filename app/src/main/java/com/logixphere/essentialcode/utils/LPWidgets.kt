d16fb52c21c582bd5ad449974495add7058fce9e
Dika Alfarell <al.ramdhan312@gmail.com>
2025-10-02 23:47:39 +0700

    optimize import

--- app/src/main/java/com/logixphere/essentialcode/utils/LPWidgets.kt
+++ app/src/main/java/com/logixphere/essentialcode/utils/LPWidgets.kt
@@ -18,8 +18,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.logixphere.essentialcode.ui.theme.Purple40
-import com.logixphere.essentialcode.viewmodels.AuthViewModel
-import com.logixphere.essentialcode.viewmodels.LoginEvent
 
 @Composable
 fun LPTextField(
